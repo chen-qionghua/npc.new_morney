047428eccb8c0e9a9fef7105d29a4a6419b64aec
chengqionghua <1169121469@qq.com>
2021-10-15 01:38:32 +0800

    添加编辑页面并给标签数据添加id于Label.vue

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,8 +1,12 @@
 const localStorageKeyName = 'tagList'
+type tags = {
+  id:string;
+  name:string
+}
 type TagListModel = {
-  data:string[],
-  fetch:() => string[],
-  create:(name:string) => 'success' | 'duplicated', //联合类型，字符串的子类型
+  data:tags[],
+  fetch:() => tags[],
+  create:(name:string) => 'success' | 'duplicated', //联合类型，字符串的子类型,方便后续类型检查（表单校验）
   save:() => void
 }
 const tagListModel:TagListModel = {
@@ -14,12 +18,16 @@ const tagListModel:TagListModel = {
   },
   create(name){
     //创建标签名的前提是拿到data，即用户输入的标签名，从fetch获取
-    if(this.data.indexOf(name)>=0) {
+    //此时data为数组对象，如this.data = [{id:1,name:2},{id:2,name:2}]，
+    //需要拿到其对象中的属性name的值，使用 map 方法收集names
+    const names = this.data.map(item => item.name);
+    if(names.indexOf(name)>=0) {
       return 'duplicated'
     }
-    this.data.push(name);
-    this.save();
-    return name  //习惯将创建的东西 return回去
+    this.data.push({id:name,name:name});
+    this.save();   //注意保存
+    return 'success';
+      //习惯将创建的东西 return回去
   },
   save() {
     window.localStorage.setItem('localStorageKeyName',JSON.stringify(this.data))
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+编辑页面
+  </Layout>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import {tagListModel} from '@/models/tagListModel'
+
+@Component
+export default class EditLabel extends Vue {
+  created() {
+    //习惯将hash声明为id常量
+    const id = this.$route.params.id;
+    tagListModel.fetch();
+    const tags = tagListModel.data;
+    //如何获取id（hash）所对应的tag
+    const tag =tags.filter(t => t.id === id)[0] //filter方法返回值为数组，必须后加[0]
+    if(tag) {
+      console.log(tag)
+    }else {
+      this.$router.replace('/404')//为防止用户404回退不了故用replace替代push
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
