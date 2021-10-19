2a9101925b0ffaf70a9a541c98ef3b8be3af4711
chengqionghua <1169121469@qq.com>
2021-10-19 23:05:58 +0800

    重新封装recordListModel.vue

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,14 +1,16 @@
 const localStorageKeyName = 'recordList'
 const recordListModel = {
+  data:[] as RecordItem[],
   clone(data:RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(data))
   },
   fetch(){
-    return JSON.parse(window.localStorage.getItem('localStorageKeyName') || '[]') as RecordItem[];
+    this.data= JSON.parse(window.localStorage.getItem('localStorageKeyName') || '[]') as RecordItem[];
+    return this.data;
     //返回值也需要指定类型，可强制指定
   },
-  save(date:RecordItem[]) {
-    window.localStorage.setItem('localStorageKeyName',JSON.stringify(date))
+  save() {
+    window.localStorage.setItem('localStorageKeyName',JSON.stringify(this.data))
   }
 }
 export {recordListModel}
