64e991fadc8209ac6176f15072a40040d18fd5f0
chengqionghua <1169121469@qq.com>
2021-10-26 23:32:34 +0800

    删除了 Types.vue

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -30,13 +30,12 @@
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
-import Types from '@/components/Money/Types.vue'
 import Tabs from '@/components/Tabs.vue'
 import intervalList from '@/constants/intervalList'
 import recordTypeList from '@/constants/recordTypeList'
 
 @Component({
-  components:{Types,Tabs}
+  components:{Tabs}
 })
 export default class Statistics extends Vue {
   type='-'
