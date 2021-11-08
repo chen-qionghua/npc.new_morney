441604a760f332c4fb5931c50217fe38163f0e01
chengqionghua <1169121469@qq.com>
2021-11-08 23:13:48 +0800

    自动更新 option

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -5,21 +5,26 @@
 </template>
 
 <script lang="ts">
-import {Component, Prop,Vue} from 'vue-property-decorator';
+import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
 import * as echarts from 'echarts'
 import EChartOption from 'echarts'
+
 @Component
 export default class Chart extends Vue {
 @Prop() option:EChartOption;
+chart:echarts
 
 mounted() {
   if(this.option === undefined) {
     return console.error('option为空')
   }else {
-
-    const chart =  echarts.init(this.$refs.wrapper as HTMLDivElement)
-    chart.setOption(this.option)
+    this.chart = echarts.init(this.$refs.wrapper as HTMLDivElement)
+    this.chart.setOption(this.option)
   }
+}
+@Watch('option')
+  onOptionChange(newValue:EChartOption) {
+  this.chart.setOption(newValue)
 }
 }
 </script>
